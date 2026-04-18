d60292f5a85baa4bd6b5e78d59ed97d1600988c8
Marko Koschak <marko.koschak@tisno.de>
2026-04-18 13:33:24 +0200

    Remove test case again - they have no influence on coverage

--- korlibs-jseval/test/korlibs/io/lang/CommonJsEvalTest.kt
+++ korlibs-jseval/test/korlibs/io/lang/CommonJsEvalTest.kt
@@ -30,23 +30,4 @@ class CommonJsEvalTest {
         val array = JSEval("var array = new Uint8Array(16); array[3] = 7; return array;")
         assertEquals(7.0, JSEval.expr("array[3]", "array" to array))
     }
-//* Not working on native
-    @Test
-    fun testInterface() = runTest {
-
-        if (!JSEval.available) return@runTest
-        class TestInterface : IJSEval {
-            override val available: Boolean get() = true
-            override val globalThis: Any? get() = JSEval.globalThis
-            override operator fun invoke(code: String, params: Map<String, Any?>): Any? = JSEval(code, params)
-        }
-
-        val testInterface = TestInterface()
-        assertEquals(true, testInterface.available)
-        assertEquals(10.0, testInterface("return a * b;", "a" to 2, "b" to 5))
-        assertEquals(15.0, testInterface.expr("a * b", "a" to 3, "b" to 5))
-        assertEquals(20.0, testInterface.exprSuspend("a * b", "a" to 4, "b" to 5))
-        assertEquals(25.0, testInterface.invokeSuspend("return a * b;", "a" to 5, "b" to 5))
-    }
-// */
 }
